a0f13c7b5cf822b78c7a028a07793e734cdd9056
Yjaswanthreddy <yjaswanthjessi@gmail.com>
2026-03-11 16:28:38 +0530

    initial commit

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -12,10 +12,11 @@ test("contact page submit", async ({page}) =>{
   const email = page.getByPlaceholder(/Enter/);
   const msg = page.locator('#userMessage');
   const btn = page.getByText('Submit');
+  // const btn = page.locator('/html/body/div/div/div/form/button');
 
 
-  email.fill('jaswanth@gmail.com');
-  msg.fill('asdfghjklk');
+  await email.fill('jaswanth@gmail.com');
+  await msg.fill('asdfghjklk');
 
   await btn.click(); 
   await msg.screenshot({type: 'png', path: './msg.png'});
@@ -23,18 +24,25 @@ test("contact page submit", async ({page}) =>{
   await page.screenshot({fullPage: true, type: 'png', path: './meow.png'});
 });
 
-// test('test', async ({ page }) => {
-//   await page.goto('http://');
-//   await page.getByRole('textbox', { name: 'Email address' }).click();
-//   await page.getByRole('textbox', { name: 'Email address' }).fill('asdfgbnmnbvx');
-//   await page.getByTestId('mytext').click();
-//   await page.getByTestId('mytext').fill('sfgnhgfdsdfgh');
-//   page.once('dialog', dialog => {
-//     console.log(`Dialog message: ${dialog.message()}`);
-//     dialog.dismiss().catch(() => {});
-//   });
-//   await page.getByRole('button', { name: 'Submit' }).click();
-// });
+[
+  {email: "jaswanth@gmail.com", msg: "hello jessei"},
+  {email: "sony@gmail.com", msg: "hello sony"}
+].forEach(obj => {
+  test(`localhost 3000 contact test for ${obj.email}`, async ({ page }) => {
+  await page.goto('http://localhost:3000/contact.html');
+  await page.getByRole('textbox', { name: 'Enter your email' }).click();
+  await page.getByRole('textbox', { name: 'Enter your email' }).fill(obj.email);
+  await page.locator('#userMessage').click();
+  await page.locator('#userMessage').fill(obj.msg);
+  page.once('dialog', dialog => {
+    console.log(`Dialog message: ${dialog.message()}`);
+    dialog.dismiss().catch(() => {});
+  });
+  await page.getByRole('button', { name: 'Submit' }).click();
+});
+
+})
+
 
 
 
