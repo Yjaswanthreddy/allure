aaf6135bc64d56ecd488b6517c2289a3581fa494
Yjaswanthreddy <yjaswanthjessi@gmail.com>
2026-03-18 22:29:16 +0530

    v2

--- tests/ui-site1.spec.js
+++
@@ -1,8 +0,0 @@
-const { test, expect } = require('@playwright/test');
-
-test.describe('Intermediate UI Components & Alerts', () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto('https://practice-automation.com/?utm_source=copilot.com');
-  });
-      
-})
